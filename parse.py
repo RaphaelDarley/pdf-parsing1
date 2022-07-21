6afb488361d7941484180e111b8898a09a8597fa
Raphael Darley <raphael@raphaeldarley.com>
2022-07-21 17:11:38 +0100

    process rasters and images separately

--- parse.py
+++ parse.py
@@ -10,22 +10,37 @@ out_dir = "out"
 
 
 def process_doc(name):
+    process_pages(name, process_drawings)
+    process_pages(name, process_rasters)
+
+
+def process_pages(name, fun):
     doc = fitz.open(f"{source_dir}/{name}")
     doc_name = os.path.basename(doc.name)
-    Path(f"{out_dir}/{doc_name}/").mkdir(exist_ok=True, parents=True)
+    Path(f"{out_dir}/{doc_name}/drawings/").mkdir(exist_ok=True, parents=True)
+    Path(f"{out_dir}/{doc_name}/rasters/").mkdir(exist_ok=True, parents=True)
     for page in doc.pages():
-        process_page(page)
+        try:
+            fun(page)
+        except Exception as e:
+            print(f"Error {e} in {fun.__name__} on file: {name}")
 
 
-def process_page(page):
-    # process_drawings(page)
-    process_images(page)
+# def process_page(page):
+#     # process_drawings(page)
+#     process_rasters(page)
 
 
-def process_images(page):
+def process_rasters(page):
     images = page.get_images()
-    ocr_text = page.get_ocr_text()
-    for image in images:
+    doc_name = os.path.basename(page.parent.name)
+    base_path = f"{out_dir}/{doc_name}/rasters/{page.number}"
+    for index, image in enumerate(images):
+        # print(page.get_image_rects(image[0]))
+        page.set_cropbox(page.get_image_rects(
+            image[0])[0].intersect(page.mediabox))
+        save_info(f"{base_path}-{index+1}",
+                  page.get_pixmap(matrix=fitz.Matrix(2, 2)), page.get_text())
 
 
 def process_drawings(page):
@@ -73,14 +88,18 @@ def process_drawings(page):
     page.set_cropbox(bound)
     img = page.get_pixmap(matrix=fitz.Matrix(2, 2))
     doc_name = os.path.basename(page.parent.name)
-    dir_path = f"{out_dir}/{doc_name}/"
-
-    img.save(f"{dir_path}{page.number}.png")
+    base_path = f"{out_dir}/{doc_name}/drawings/{page.number}"
     img_txt = page.get_textbox(bound)
-    with open(f"{dir_path}{page.number}.img.txt", "w", encoding="utf-8") as f:
-        f.write(img_txt)
-    with open(f"{dir_path}{page.number}.page.txt", "w", encoding="utf-8") as f:
-        f.write(page.get_text())
+    save_info(base_path, img, page.get_text(), img_txt=img_txt)
+
+
+def save_info(base_path, img, page_txt, img_txt=False):
+    img.save(f"{base_path}.png")
+    if not img_txt == False:
+        with open(f"{base_path}.img.txt", "w", encoding="utf-8") as f:
+            f.write(img_txt)
+    with open(f"{base_path}.page.txt", "w", encoding="utf-8") as f:
+        f.write(page_txt)
 
 
 if __name__ == '__main__':
@@ -90,6 +109,11 @@ if __name__ == '__main__':
     for num, name in enumerate(files):
         process_doc(name)
         print(f"finished {num +1}/{len(files)}: {name}")
+
+        doc = fitz.open(f"{source_dir}/{name}")
+        print(name)
+        print(doc.metadata)
+
         break
 
     # for page_num in range(1, 27, 1):
